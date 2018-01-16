bed4354cd0a59b97b4e17faf913a38236c76bfdf
mr-akin <mr-akin@users.github.com>
2018-01-16 21:24:56 +0530

    Parsimonius grammer: recursion not working

--- test2parsi.py
+++ test2parsi.py
@@ -1,7 +1,22 @@
 from parsimonious.grammar import Grammar
+from parsimonious import *
+parser = Grammar(r"""
+    styled_text = bold_text / italic_text
+    bold_text   = "((" text "))"
+    italic_text = "''" text "''"
+    text        = ~"[A-Z 0-9]*"i
+    """)		
+#	E=E "++" E/"("E")"/id
 
-g1=Grammer("""hello='hello'""")
-		
-
+grammar_expr='''
+	E=p 
+	p= "++" p / r
+	r=id
+	id=~"[0-9]*"
+	'''			
+parser2=Grammar(grammar_expr)
 				
+code = input("INP: ")
 
+
+print(parser2.parse(code))
