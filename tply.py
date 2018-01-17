a61c7d1c14fe0abfeaaa4dd7da679efaea5f8491
mr-akin <mr-akin@users.github.com>
2018-01-17 21:52:07 +0530

    PLy using lex to get all tokens

--- tply.py
+++ tply.py
@@ -4,10 +4,31 @@ tokens=(
     "number",
     "string"
 )
+
+
 t_print=r'print[\s]+'
 t_number=r'\d+'
-t_string=r'.+'
+t_string=r'[A-Z a-z \s]+'
+
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
 lexer=lex.lex()
 inp=input('INP: ')
 lexer.input(inp)
-print(lexer.token())
+while True:
+    tok = lexer.token()
+    if not tok:
+        break      # No more input
+    print(tok)
